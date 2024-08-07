c7ebbd97366c49e822195a14abee6acbb98fc2bf
Fwzryy <raihan0603@gmail.com>
2024-08-07 13:39:34 +0700

    Add Time Travel

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react"
 
 // Kotak tictactoe
@@ -55,17 +56,35 @@ function Board({xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)]) //[][]
-  const currentSquares = history[history.length -1 ];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0; //x
+  const currentSquares = history[currentMove];
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+    ; //~-~-
+  }
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
   }
 
   const moves = history.map((squares,move) => {
-    let description = ''
+    let description = '';
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
   });
 
   return (
@@ -74,7 +93,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-          <ol>{/* Todo */}</ol>
+          <ol>{moves}</ol>
       </div>
     </div>
   )
